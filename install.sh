895d51cca8fcddfab4612dd41c1abb1500909944
boodjoom <boodjoom@gmail.com>
2016-11-29 05:58:39 -0500

    disabled autorun service

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 echo "Create service"
 cp "service.sh" "/etc/init.d/RoverTelemetry"
 chmod +x /etc/init.d/RoverTelemetry
-echo "Install to run at boot time.."
-update-rc.d RoverTelemetry defaults
+#echo "Install to run at boot time.."
+#update-rc.d RoverTelemetry defaults
